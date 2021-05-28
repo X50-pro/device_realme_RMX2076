1f95d989a264194b0415d1f66f1312a0cef83ec0
lp-reborn <Scissordragonboy1@gmail.com>
2021-05-28 17:46:31 +0000

    RMX2076: add assert for Realme x50 pro varients

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,4 +10,4 @@ DEVICE_PATH := device/xiaomi/umi
 -include device/xiaomi/sm8250-common/BoardConfigCommon.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := umi
+TARGET_OTA_ASSERT_DEVICE := RMX2076 RMX2075 RMX2071 RMX2071CN RMX2076PU RMX2071PU
