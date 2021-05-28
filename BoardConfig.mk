b7c695888426ee4d264d4c0b307f99433d9bec39
lp-reborn <Scissordragonboy1@gmail.com>
2021-05-28 17:43:04 +0000

    umi: drop prebuilt kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,9 +11,3 @@ DEVICE_PATH := device/xiaomi/umi
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := umi
-
-# Prebuilt Core
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
-
